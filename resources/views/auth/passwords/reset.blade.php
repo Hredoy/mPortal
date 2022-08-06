ff01a1166707d9b1ac084e4d0693eda494329212
dev-shakib <mehedi@lskit.com>
2022-08-06 12:10:49 +0600

    git ignore updated

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,42 +1,55 @@
-@extends('layouts.app')
+@extends('frontend.layout.app')
+@push('custom_css')
+    <style>
+        .login-wraper .login-window {
+            top: 15%;
+            left: 30%;
+            margin-top: 0px;
+        }
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <form method="POST" action="{{ route('password.request') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
-
+        @media screen and (max-width:767px) {
+            .login-wraper .login-window {
+                left: 0%;
+            }
+        }
+    </style>
+@endpush
+@section('main_section')
+    <div class="container-fluid bg-image">
+        <div class="row">
+            <div class="login-wraper">
+                <div class="hidden-xs">
+                    <img src="{{ asset('assets/frontend/images/login.jpg') }}" alt="">
+                </div>
+                <div class="login-window">
+                    <div class="l-head">
+                        Reset Your Password
+                    </div>
+                    <div class="l-form">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <form method="POST" action="{{ route('password.request') }}">
+                            @csrf
+                            <input type="hidden" name="token" value="{{ $token }}">
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input id="email" type="email"
+                                    class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                                    value="{{ old('email') }}" required autofocus placeholder="sample@gmail.com">
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input id="password" type="password"
+                                    class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                                    required placeholder="**********">
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback">
@@ -44,12 +57,11 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}" name="password_confirmation" required>
+                            <div class="form-group">
+                                <label for="password-confirm">Confirm Password</label>
+                                <input id="password-confirm" type="password"
+                                    class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}"
+                                    name="password_confirmation" required placeholder="**********">
 
                                 @if ($errors->has('password_confirmation'))
                                     <span class="invalid-feedback">
@@ -57,19 +69,18 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
+                            <div class="row">
+                                <div class="col-lg-7"><button type="submit" class="btn btn-cv1">Reset Password</button>
+                                </div>
+                                <div class="hidden-xs">
+                                    <div class="col-lg-1 ortext">or</div>
+                                    <div class="col-lg-4 signuptext"><a href="{{ route('login') }}">Sign In</a></div>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,124 +1,139 @@
 @extends('frontend.layout.app')
 @push('custom_css')
     <style>
-        .login-wraper>.hidden-xs>img{
+        .login-wraper>.hidden-xs>img {
             height: 1000px;
         }
-        .login-window{
+
+        .login-window {
             top: 35% !important;
         }
     </style>
 @endpush
 @section('main_section')
-<div class="container-fluid bg-image">
-    <div class="row">
-        <div class="login-wraper">
-            <div class="hidden-xs">
-                <img src="{{asset('assets/frontend/images/login.jpg')}}" alt="">
-            </div>
-            <div class="banner-text hidden-xs">
-                <div class="line"></div>
-                <div class="b-text">
-                    Watch <span class="color-active">millions<br> of</span> <span class="color-b1">v</span><span class="color-b2">i</span><span class="color-b3">de</span><span class="color-active">os</span> for free.
+    <div class="container-fluid bg-image">
+        <div class="row">
+            <div class="login-wraper">
+                <div class="hidden-xs">
+                    <img src="{{ asset('assets/frontend/images/login.jpg') }}" alt="">
                 </div>
-                <div class="overtext">
-                    Over 6000 videos uploaded Daily.
+                <div class="banner-text hidden-xs">
+                    <div class="line"></div>
+                    <div class="b-text">
+                        Watch <span class="color-active">millions<br> of</span> <span class="color-b1">v</span><span
+                            class="color-b2">i</span><span class="color-b3">de</span><span class="color-active">os</span>
+                        for free.
+                    </div>
+                    <div class="overtext">
+                        Over 6000 videos uploaded Daily.
+                    </div>
                 </div>
-            </div>
-            <div class="login-window">
-                <div class="l-head">
-                    Sign Up for Free
-                </div>
-                <div class="l-form">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-                        <div class="form-group">
-                            <label for="name">Username</label>
-                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus placeholder="johndoe">
+                <div class="login-window">
+                    <div class="l-head">
+                        Sign Up for Free
+                    </div>
+                    <div class="l-form">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Username</label>
+                                <input id="name" type="text"
+                                    class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"
+                                    value="{{ old('name') }}" required autofocus placeholder="johndoe">
 
                                 @if ($errors->has('name'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
-                        </div>
-                        <div class="form-group">
-                            <label for="first_name">First Name</label>
-                            <input id="first_name" type="text" class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name" value="{{ old('first_name') }}" required placeholder="John">
+                            </div>
+                            <div class="form-group">
+                                <label for="first_name">First Name</label>
+                                <input id="first_name" type="text"
+                                    class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}"
+                                    name="first_name" value="{{ old('first_name') }}" required placeholder="John">
 
                                 @if ($errors->has('first_name'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('first_name') }}</strong>
                                     </span>
                                 @endif
-                        </div>
-                        <div class="form-group">
-                            <label for="last_name">Last Name</label>
-                            <input id="last_name" type="text" class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name" value="{{ old('last_name') }}" required placeholder="Doe">
+                            </div>
+                            <div class="form-group">
+                                <label for="last_name">Last Name</label>
+                                <input id="last_name" type="text"
+                                    class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}"
+                                    name="last_name" value="{{ old('last_name') }}" required placeholder="Doe">
 
                                 @if ($errors->has('last_name'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('last_name') }}</strong>
                                     </span>
                                 @endif
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required placeholder="sample@gmail.com">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input id="email" type="email"
+                                    class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                                    value="{{ old('email') }}" required placeholder="sample@gmail.com">
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="**********">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input id="password" type="password"
+                                    class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                                    required placeholder="**********">
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                        </div>
-                        <div class="form-group">
-                            <label for="password-confirm">Re-type Password</label>
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required placeholder="**********">
-                        </div>
-                        @if(config('settings.reCaptchStatus'))
+                            </div>
                             <div class="form-group">
-                                <div class="col-sm-6 col-sm-offset-4">
-                                    <div class="g-recaptcha" data-sitekey="{{ config('settings.reCaptchSite') }}"></div>
+                                <label for="password-confirm">Re-type Password</label>
+                                <input id="password-confirm" type="password" class="form-control"
+                                    name="password_confirmation" required placeholder="**********">
+                            </div>
+                            @if (config('settings.reCaptchStatus'))
+                                <div class="form-group">
+                                    <div class="col-sm-6 col-sm-offset-4">
+                                        <div class="g-recaptcha" data-sitekey="{{ config('settings.reCaptchSite') }}">
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+                            <div class="row">
+                                <div class="col-lg-7"><button type="submit" class="btn btn-cv1">Sign Up</button></div>
+                                <div class="hidden-xs">
+                                    <div class="col-lg-1 ortext">or</div>
+                                    <div class="col-lg-4 signuptext"><a href="{{ route('login') }}">Log In</a></div>
                                 </div>
                             </div>
-                        @endif
-                        <div class="row">
-                            <div class="col-lg-7"><button type="submit" class="btn btn-cv1">Sign Up</button></div>
-                            <div class="hidden-xs">
-                                <div class="col-lg-1 ortext">or</div>
-                                <div class="col-lg-4 signuptext"><a href="{{route('login')}}">Log In</a></div>
-                            </div>
-                        </div>
-                        {{-- <div class="row hidden-xs">
+                            {{-- <div class="row hidden-xs">
                             <div class="col-lg-12 forgottext">
                                 <a href="#">By clicking "Sign Up" I agree to circle's Terms of Service.</a>
                             </div>
                         </div> --}}
-                        <div class="visible-xs text-center mt-30">
-                            <span class="forgottext"><a href="{{route('login')}}">Already have an account?</a></span>
-                            <span class="signuptext"><a href="{{route('login')}}">Login here</a></span>
-                        </div>
-                    </form>
+                            <div class="visible-xs text-center mt-30">
+                                <span class="forgottext"><a href="{{ route('login') }}">Already have an account?</a></span>
+                                <span class="signuptext"><a href="{{ route('login') }}">Login here</a></span>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('custom_script')
-    @if(config('settings.reCaptchStatus'))
+    @if (config('settings.reCaptchStatus'))
         <script src='https://www.google.com/recaptcha/api.js'></script>
     @endif
 @endsection
