d0e3c3cccf3a92675b03916b07ae4b4eaa412d55
nextgenbite <iliusrahman@gmail.com>
2022-08-06 17:39:33 +0600

    like done

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Upload;
 use App\Models\Comment;
 use App\Models\Like;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -17,7 +18,8 @@ class HomeController extends Controller
     public function singleVideo($id){
        $upload = Upload::findOrFail($id);
        $comments = Comment::whereUpload_id($id)->get();
-       $likeCount = Like::select('count')->count();
-        return view('frontend.single_video', compact('upload', "comments", "likeCount"));
+       $likeCount = Like::whereUpload_id($id)->select('count')->count();
+       $likeCheck = Like::where('user_id',Auth::id())->where('upload_id',$id)->first();
+        return view('frontend.single_video', compact('upload', "comments", "likeCount", "likeCheck"));
     }
 }
--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -5,53 +5,29 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Like;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    // public function index()
-    // {
-    //     $likes =Like::with('uploads')->get();
-    //     return view('backend.like.index', compact('likes'));
-    // }
 
-    // /**
-    //  * Show the form for creating a new resource.
-    //  *
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function create()
-    // {
-    //     //
-    // }
+    public function unlike($id)
+    {
+        Like::where('user_id',Auth::id())->delete();
 
-    // /**
-    //  * Store a newly created resource in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @return \Illuminate\Http\Response
-    //  */
-    public function store(Request $request, $id)
+        return redirect()->back()->with('success', 'succeccfully unliked');
+    }
+    public function store($id)
     {
         if (Auth::check()) {
 
             $exists = Like::where('user_id',Auth::id())->where('upload_id',$id)->first();
-            $count = 1;
             if (!$exists) {
                 Like::create([
                 'user_id' => Auth::id(),
                 'upload_id' => $id,
-                'count' => $count++,
+                'count' => 1,
             ]);
                 return redirect()->back()->with('success', 'succeccfully liked this video.');
-
-            }else{
-                return redirect()->back()->with('error', 'You Already liked this video.');
-
             }
 
         }else{
@@ -62,89 +38,5 @@ class LikeController extends Controller
 
     }
 
-    // /**
-    //  * Display the specified resource.
-    //  *
-    //  * @param  \App\Models\like  $like
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function show($id)
-    // {
-    //     Like::findOrFail($id);
-    //     return view('backend.like.show');
-    // }
 
-    // /**
-    //  * Show the form for editing the specified resource.
-    //  *
-    //  * @param  \App\Models\like  $like
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function edit($id)
-    // {
-    //    $like = Like::findOrFail($id);
-    //     return view('backend.like.edit', compact('like'));
-    // }
-
-    // /**
-    //  * Update the specified resource in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @param  \App\Models\like  $like
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function update(Request $request, $id)
-    // {
-    //     Like::findOrFail($id)->update(($request->all()));
-    //     return redirect()->back()->with('update', 'like has been update successfully.');
-    // }
-
-    // /**
-    //  * Remove the specified resource from storage.
-    //  *
-    //  * @param  \App\Models\like  $like
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function destroy($id)
-    // {
-    //     Like::findOrFail($id)->delete();
-    //     return redirect()->back()->with('delete', 'like has been delete successfully.');
-    // }
-
-    public function Addlike(Request $request, $id)
-    {
-        if (Auth::check()) {
-
-            $exists = Like::where('user_id',Auth::id())->where('upload_id',$id)->first();
-            $count = 1;
-            if (!$exists) {
-                Like::create([
-                'user_id' => Auth::id(),
-                'upload_id' => $id,
-                'count' => $count++,
-            ]);
-           return response()->json(['success' => 'Successfully Added On Your Wishlist']);
-
-            }else{
-
-                return response()->json(['error' => 'You Already liked this video']);
-
-            }
-
-        }else{
-
-            return response()->json(['error' => 'At First Login Your Account']);
-
-        }
-    }
-    public function getwishlist()
-    {
-        $wishlist= Wishlist::where('user_id',Auth::id())->with('product')->get();
-        return response()->json($wishlist);
-    }
-    public function removewishlist($id)
-    {
-        Wishlist::where('user_id',Auth::id())->where('id',$id)->delete();
-		return response()->json(['success' => 'Successfully Product Remove']);
-    }
 }
