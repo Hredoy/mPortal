1777cf005837847a0f556aaba15f46b9541e566b
Faruq Hossen <faruqhossen30@gmail.com>
2022-08-13 15:23:15 +0600

    Video duration calculate on Upload model added

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -31,8 +31,8 @@ class UploadController extends Controller
         $page_title = 'Create A New Upload';
         $categories = Category::all();
         // $regions = Region::all();
-        $countries =CountryListFacade::getList('en');
-        return view('backend.uploads.create', compact('page_title', 'countries','categories'));
+        $countries = CountryListFacade::getList('en');
+        return view('backend.uploads.create', compact('page_title', 'countries', 'categories'));
     }
 
     /**
@@ -57,7 +57,7 @@ class UploadController extends Controller
             $fileName = $request->file('thumbnail_image')->getClientOriginalExtension();
             if ($fileName == 'jpg' || $fileName == 'png' || $fileName == 'jpeg' || $fileName == 'gif' || $fileName == 'svg') {
                 $uploadPath = 'uploads/' . Auth::user()->name . '/images/';
-                $newFileName =$uploadPath.time() . '.' . $fileName;
+                $newFileName = $uploadPath . time() . '.' . $fileName;
                 $request->thumbnail_image->move($uploadPath, $newFileName);
             }
         }
@@ -66,18 +66,22 @@ class UploadController extends Controller
             if ($upload_extension == 'mp3') {
                 $type = 1;
                 $uploadPath = 'uploads/' . Auth::user()->name . '/audio/';
-                $uploadFileName = $uploadPath.time() . '.' . $upload_extension;
+                $uploadFileName = $uploadPath . time() . '.' . $upload_extension;
                 $request->upload->move($uploadPath, $uploadFileName);
-
             } elseif ($upload_extension == 'mp4' || $upload_extension == '3gp' || $upload_extension == 'mpeg') {
                 $type = 2;
                 $uploadPath = 'uploads/' . Auth::user()->name . '/video/';
-                $uploadFileName = $uploadPath.time() . '.' . $upload_extension;
+                $uploadFileName = $uploadPath . time() . '.' . $upload_extension;
                 $request->upload->move($uploadPath, $uploadFileName);
-            }else {
+            } else {
                 $type = 3;
             }
+            // For calculate file duration
+            $getID3 = new \getID3;
+            $durationtime =  $getID3->analyze($uploadFileName);
+            $duration =  $durationtime['playtime_string'];
         }
+
 
         $upload = new Upload;
         $upload->name = $request->name;
@@ -89,7 +93,7 @@ class UploadController extends Controller
         $upload->release_date = $request->release_date;
         $upload->region = $request->region;
         $upload->type_id = $type;
-        // $upload->upload_duration = $duration;
+        $upload->upload_duration = $duration;
         $upload->save();
         return redirect()->route('public.upload')->with('create', 'File has been created successfully.');
     }
@@ -115,7 +119,7 @@ class UploadController extends Controller
     {
         $page_title = 'Update A New Upload';
         $upload = Upload::find($id);
-        $countries =CountryListFacade::getList('en');
+        $countries = CountryListFacade::getList('en');
         return view('backend.uploads.edit', compact('page_title', 'upload', 'countries'));
     }
 
@@ -139,52 +143,56 @@ class UploadController extends Controller
         ]);
 
 
-          $upload = Upload::findOrFail($id);
+        $upload = Upload::findOrFail($id);
 
-          $input = $request->all();
+        $input = $request->all();
 
-          $newFileName = '';
-          if ($request->hasFile('thumbnail_image')) {
+        $newFileName = '';
+        if ($request->hasFile('thumbnail_image')) {
             if (file_exists(public_path($upload->thumbnail_image))) {
                 unlink(public_path($upload->thumbnail_image));
             }
             $fileName = $request->file('thumbnail_image')->getClientOriginalExtension();
             if ($fileName == 'jpg' || $fileName == 'png' || $fileName == 'jpeg' || $fileName == 'gif' || $fileName == 'svg') {
                 $uploadPath = 'uploads/' . Auth::user()->name . '/images/';
-                $newFileName = $uploadPath.time() . '.' . $fileName;
+                $newFileName = $uploadPath . time() . '.' . $fileName;
                 $request->thumbnail_image->move($uploadPath, $newFileName);
             }
-            $input['thumbnail_image']= $newFileName;
+            $input['thumbnail_image'] = $newFileName;
         }
-      //FOR VIDEO UPDATE
-      if ($request->hasFile('upload')) {
+        //FOR VIDEO UPDATE
+        if ($request->hasFile('upload')) {
 
-        if (file_exists(public_path($upload->upload))) {
-            unlink(public_path($upload->upload));
-        }
+            if (file_exists(public_path($upload->upload))) {
+                unlink(public_path($upload->upload));
+            }
 
 
-        $video_Audio_extension = $request->file('upload')->getClientOriginalExtension();
-        if ($video_Audio_extension == 'mp3') {
-            $input['type'] = 1;
-            $uploadPath = 'uploads/' . Auth::user()->name . '/audio/';
-            $audioVideoFileName = $uploadPath.time() . '.' . $video_Audio_extension;
-            $request->video->move($uploadPath, $audioVideoFileName);
-            $input['upload']= $audioVideoFileName;
-        } elseif ($video_Audio_extension == 'mp4' || $video_Audio_extension == '3gp' || $video_Audio_extension == 'mpeg') {
-            $input['type'] = 2;
-            $uploadPath = 'uploads/' . Auth::user()->name . '/video/';
-                $uploadFileName = $uploadPath.time() . '.' . $video_Audio_extension;
+            $video_Audio_extension = $request->file('upload')->getClientOriginalExtension();
+            if ($video_Audio_extension == 'mp3') {
+                $input['type'] = 1;
+                $uploadPath = 'uploads/' . Auth::user()->name . '/audio/';
+                $audioVideoFileName = $uploadPath . time() . '.' . $video_Audio_extension;
+                $request->video->move($uploadPath, $audioVideoFileName);
+                $input['upload'] = $audioVideoFileName;
+            } elseif ($video_Audio_extension == 'mp4' || $video_Audio_extension == '3gp' || $video_Audio_extension == 'mpeg') {
+                $input['type'] = 2;
+                $uploadPath = 'uploads/' . Auth::user()->name . '/video/';
+                $uploadFileName = $uploadPath . time() . '.' . $video_Audio_extension;
                 $request->upload->move($uploadPath, $uploadFileName);
-            $input['upload'] = $uploadFileName;
-        }else{
-            $input['type'] = 3;
+                $input['upload'] = $uploadFileName;
+            } else {
+                $input['type'] = 3;
+            }
+            // For calculate file duration
+            $getID3 = new \getID3;
+            $durationtime =  $getID3->analyze($uploadFileName);
+            $duration =  $durationtime['playtime_string'];
+            $input['upload_duration'] = $duration;
         }
+        $upload->update($input);
 
-    }
-      $upload->update($input);
-
-          return redirect()->back()->with('create', 'File has been update successfully.');
+        return redirect()->back()->with('create', 'File has been update successfully.');
     }
 
     /**
@@ -195,7 +203,7 @@ class UploadController extends Controller
      */
     public function destroy($id)
     {
-        $delete=  Upload::findOrFail($id);
+        $delete =  Upload::findOrFail($id);
         if (file_exists(public_path($delete->upload))) {
             unlink(public_path($delete->upload));
         }
@@ -207,26 +215,26 @@ class UploadController extends Controller
     }
     public function getMusic()
     {
-       $page_title = 'Music Management';
-       $empty_message = 'No Music is available.';
-       $uploads = Upload::where([['category_id', 1],['user_id', Auth::user()->id]] )->with('categories')->get();
-       $roles = Role::all();
-       return View('backend.uploads.index', compact('uploads', 'roles', 'page_title', 'empty_message'));
+        $page_title = 'Music Management';
+        $empty_message = 'No Music is available.';
+        $uploads = Upload::where([['category_id', 1], ['user_id', Auth::user()->id]])->with('categories')->get();
+        $roles = Role::all();
+        return View('backend.uploads.index', compact('uploads', 'roles', 'page_title', 'empty_message'));
     }
     public function comedy()
     {
-       $page_title = 'Comedy Management';
-       $empty_message = 'No Comedy is available.';
-       $uploads = Upload::where([['category_id', 2],['user_id', Auth::user()->id]]  )->with('categories')->get();
-       $roles = Role::all();
-       return View('backend.uploads.index', compact('uploads', 'roles', 'page_title', 'empty_message'));
+        $page_title = 'Comedy Management';
+        $empty_message = 'No Comedy is available.';
+        $uploads = Upload::where([['category_id', 2], ['user_id', Auth::user()->id]])->with('categories')->get();
+        $roles = Role::all();
+        return View('backend.uploads.index', compact('uploads', 'roles', 'page_title', 'empty_message'));
     }
     public function talent()
     {
-       $page_title = 'Talent Management';
-       $empty_message = 'No Comedy is available.';
-       $uploads = Upload::where([['category_id', 3],['user_id', Auth::user()->id]])->with('categories')->get();
-       $roles = Role::all();
-       return View('backend.uploads.index', compact('uploads', 'roles', 'page_title', 'empty_message'));
+        $page_title = 'Talent Management';
+        $empty_message = 'No Comedy is available.';
+        $uploads = Upload::where([['category_id', 3], ['user_id', Auth::user()->id]])->with('categories')->get();
+        $roles = Role::all();
+        return View('backend.uploads.index', compact('uploads', 'roles', 'page_title', 'empty_message'));
     }
 }
