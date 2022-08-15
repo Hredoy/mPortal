bad94b7667e7068265413851e6a874f29378e408
Faruq Hossen <faruqhossen30@gmail.com>
2022-08-15 11:30:38 +0600

    others content show added

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -124,6 +124,65 @@
                         </div>
                     </div>
                 </div>
+                {{-- For Other Contnet --}}
+                @if($others)
+                <div class="content-block head-div">
+                    <div class="cb-header">
+                        <div class="row">
+                            <div class="col-lg-10 col-sm-10 col-xs-8">
+                                <ul class="list-inline">
+                                    <li>
+                                        <a href="#" class="color-active">
+                                            <span class="visible-xs">Others</span>
+                                            <span class="hidden-xs">Others Videos</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                                {{-- <div class="btn-group pull-right">
+                                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                      <span class="glyphicon glyphicon-filter"></span>
+                                      <span class="sr-only">Filters</span>
+                                      <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                      <li><a href="{{Route("home.latest")}}">Latest</a></li>
+                                      <li><a href="{{Route("home.view")}}">Mostly View</a></li>
+                                      <li><a href="{{Route("home.like")}}">Mostly Liked</a></li>
+                                    </ul>
+                                  </div> --}}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cb-content videolist">
+                        <div class="row">
+                            @foreach ($others->where('featured', 1) as $item)
+                            <div class="col-lg-3 col-sm-6 videoitem mx-2">
+                                <div class="b-video">
+                                    <div class="v-img">
+                                        <a href="{{route('singleVideo', $item->id)}}"><img src="{{asset($item->thumbnail_image)}}" alt="" width="100%" height="215px"></a>
+                                        <div class="time">3:50</div>
+                                    </div>
+                                    <div class="v-desc">
+                                        <a href="{{route('singleVideo', $item->id)}}">{{$item->name}}</a>
+                                    </div>
+                                    <div class="v-views">
+                                        {{$item->view}} views. <span class="v-percent"><span class="v-circle"></span> 78%</span>
+                                        {{-- <div class="pull-right">
+                                            @if ( $likeChecks->upload_id == $item->id && $likeChecks->user_id == Auth::id() )
+                                            <a href="{{Route('like', $item->id)}}" class="btn "><i class="fa fa-thumbs-o-up" style="font-size: 1.2em"></i></a>
+                                            @else
+                                            <a href="{{Route('unlike', $item->id)}}" class="btn"><i class="fa fa-thumbs-o-down  " style="font-size: 1.2em"></i></a>
+                                            @endif
+                                           <small> {{$item->likes->count('count')}} Likes</small>
+                                        </div> --}}
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                @endif
                 <!-- /Featured Videos -->
 
                 <!-- New Videos in Current Region -->
